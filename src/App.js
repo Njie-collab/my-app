b2c3d836357b5ba97425f065822ae66c842a6942
Jerreh <timeless_sound@hotmail.de>
2021-10-15 10:23:52 +0200

    usestate modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import  {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [line , setLine] = useState("Post")
+  
+   useEffect(()=> {
+      fetch(`https://jsonplaceholder.typicode.com/${setLine} `)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    },[line]);
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setLine("Post")}>Post</button>
+      <button onClick={() => setLine("Users")}>Users</button>
+      <button onClick={() => setLine("Comment")}>comments</button>
     </div>
+    <h1>{line}</h1>
+    </>
+    
   );
 }
 
